fb0696be0de480778f35361b89650fa1566563fc
AntonioPtnR <antoniopr009@gmail.com>
2026-06-11 23:56:41 +0200

    fix: remove generated sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://antoniopr.pages.dev",
-	integrations: [react(), sitemap()],
+	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
